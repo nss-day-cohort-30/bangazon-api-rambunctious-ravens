26816065e82b8162a7b38b38fb37d084385ff86f
Sam Cronin <croninsw@gmail.com>
2019-05-10 09:36:01 -0500

    Completed Order Model

--- BangazonAPI/Models/Order.cs
+++ BangazonAPI/Models/Order.cs
@@ -7,5 +7,17 @@ namespace BangazonAPI.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentTypeId { get; set; }
     }
 }
+
+
+//CREATE TABLE[Order] (
+//   Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+//   CustomerId INTEGER NOT NULL,
+//	PaymentTypeId INTEGER,
+//    CONSTRAINT FK_Order_Customer FOREIGN KEY(CustomerId) REFERENCES Customer(Id),
+//    CONSTRAINT FK_Order_Payment FOREIGN KEY(PaymentTypeId) REFERENCES PaymentType(Id)
+//);
